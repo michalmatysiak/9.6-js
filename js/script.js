d585da9cd2d7f776fa00240fcd54953e59355480
Michał Matysiak <michalmatysiak02@gmail.com>
2018-03-22 18:31:35 +0100

    Add files js css

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+  var element = document.createElement('li');
+  var number = document.getElementsByTagName('li').length;
+  element.innerHTML = 'item ' + number;
+
+  list.appendChild(element);
+  console.log(element);
+});
